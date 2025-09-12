2e3a63a0420e5435d239bf9b041128188506d460
oodum <adamtam2004@outlook.com>
2025-09-12 12:06:34 -0400

    FCTween built into source

--- src/Source/Apesack_Pickleballs/Apesack_Pickleballs.Build.cs
+++ src/Source/Apesack_Pickleballs/Apesack_Pickleballs.Build.cs
@@ -2,19 +2,24 @@
 
 using UnrealBuildTool;
 
-public class Apesack_Pickleballs : ModuleRules
-{
-	public Apesack_Pickleballs(ReadOnlyTargetRules Target) : base(Target)
-	{
+public class Apesack_Pickleballs : ModuleRules {
+	public Apesack_Pickleballs(ReadOnlyTargetRules Target) : base(Target) {
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput",
+			"FCTween",
+		});
+
+		PrivateDependencyModuleNames.AddRange(new string[] { });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
+
 		// Uncomment if you are using online features
 		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
 
