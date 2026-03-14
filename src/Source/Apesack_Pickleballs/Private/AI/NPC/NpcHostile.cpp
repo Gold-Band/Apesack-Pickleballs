b873753f85e2fa6c91c5e7dff527b27f6312d837
GlobalBooga <88789784+GlobalBooga@users.noreply.github.com>
2026-03-14 10:14:19 -0400

    remove redundant code

--- src/Source/Apesack_Pickleballs/Private/AI/NPC/NpcHostile.cpp
+++ src/Source/Apesack_Pickleballs/Private/AI/NPC/NpcHostile.cpp
@@ -60,7 +60,6 @@ void ANpcHostile::CreateBehaviours()
 	FAction MeleeAttackAction{FString("Attack")};
 	MeleeAttackAction.Func = [&](const float DeltaTime){return MeleeAttack(DeltaTime);};
 	
-	
 	// Melee Attack
 	MeleeAttackTask.Actions.Add(TargetAttackableAction);
 	MeleeAttackTask.Actions.Add(MoveToAction);
@@ -76,8 +75,6 @@ void ANpcHostile::CreateBehaviours()
 	MoveForwardTask.Condition = [&] {return MoveToCondition();};
 	MoveForwardTask.bPrintDebug = bPrintDebug_MoveTo;
 	
-	
-	
 	HtnDomain->AssignTask(&MeleeAttackTask);
 	HtnDomain->AssignTask(&MoveForwardTask);
 	
@@ -90,15 +87,6 @@ void ANpcHostile::OnNearestAttackableChanged(AActor* NewTarget, EOriginSide Side
 	if (Side != MainSide) return;
 	
 	MeleeAttackTask.Reset();
-}
-
-float ANpcHostile::GetAngleBetweenVectors(const FVector& A, const FVector& B)
-{
-	if (A.IsNearlyZero() || B.IsNearlyZero()) return 0.f;
-	
-	const float Dot = FVector::DotProduct(A, B);
-	const float CrossDot = FVector::CrossProduct(A, B).Dot(FVector::UpVector);
-	return FMath::RadiansToDegrees(FMath::Atan2(CrossDot, Dot));
 }
 
 EActionState ANpcHostile::Walk(float DeltaTime)
@@ -230,11 +218,6 @@ EActionState ANpcHostile::MeleeAttack(float DeltaTime)
 	return EActionState::Succeeded;
 }
 
-bool ANpcHostile::MeleeAttackCondition() const
-{
-	return /*NpcManager->FindNearestNpc(GetActorLocation(), ENpcSearchOption::AnyFriendly, MainSide) != nullptr*/ true;
-}
-
 EActionState ANpcHostile::TargetAttackable(float DeltaTime)
 {
 	if (MainSide == EOriginSide::Left)
@@ -249,7 +232,7 @@ EActionState ANpcHostile::TargetAttackable(float DeltaTime)
 #if WITH_EDITOR
 	if (bPrintDebug_TargetNearestAny)
 	{
-		const float Angle = GetAngleBetweenVectors(GetActorLocation(), TargetActor->GetActorLocation());
+		const float Angle = ADefaultGameMode::GetAngleBetweenVectors(GetActorLocation(), TargetActor->GetActorLocation());
 		UE_LOG(LogTemp, Warning, TEXT("%s::NearestAttackable=%s (Angle=%f)"), *GetActorNameOrLabel(),TargetActor? *TargetActor->GetActorNameOrLabel(): TEXT("Null"), Angle);
 	}
 #endif
